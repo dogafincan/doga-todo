0c0e209affdc1b3c796493502e35265284b6ef04
Doga Fincan <doga@icloud.com>
2022-09-19 21:52:58 +0200

    Add hardcoded todos

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,5 +1,9 @@
-export default function Todo() {
+type Props = {
+  todo: string;
+};
+
+export default function Todo({ todo }: Props) {
   return (
-    <div className="border rounded text-3xl py-3.5 px-5 bg-white">Cucumber</div>
+    <div className="border rounded text-3xl py-3.5 px-5 bg-white">{todo}</div>
   );
 }
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,11 +1,14 @@
 import Todo from "./Todo";
 
+const initialTodos = ["🍅 Tomato", "🥒 Cucumber", "🧀 Cheese", "🥬 Lettuce"];
+
 export default function Todos() {
   return (
     <div className="flex flex-col space-y-2">
-      <Todo />
-      <Todo />
-      <Todo />
+      {initialTodos.map((todo) => (
+        // eslint-disable-next-line react/jsx-key
+        <Todo todo={todo} />
+      ))}
     </div>
   );
 }
