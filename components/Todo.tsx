41a1009addb320d8ea59c00743ad76df4f6083cd
Doga Fincan <doga@icloud.com>
2022-09-21 11:05:02 +0200

    Add edit support

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
+
 type Props = {
-  todo: string;
+  initialTodo: string;
 };
 
-export default function Todo({ todo }: Props) {
-  return (
-    <div className="flex h-20 items-center rounded-3xl bg-white py-3.5 px-8 text-2xl shadow dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none">
+export default function Todo({ initialTodo }: Props) {
+  const [todo, setTodo] = useState(initialTodo);
+  const [edit, setEdit] = useState(false);
+
+  function handleSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setEdit(!edit);
+  }
+
+  function handleClick() {
+    setEdit(!edit);
+  }
+
+  return edit ? (
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <input
+        className="h-20 rounded-3xl py-3.5 px-8 text-2xl shadow dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none"
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}
+        placeholder="Add todo..."
+        autoFocus
+      />
+    </form>
+  ) : (
+    <div
+      onClick={handleClick}
+      className="flex h-20 items-center rounded-3xl bg-white py-3.5 px-8 text-2xl shadow dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none"
+    >
       {todo}
     </div>
   );
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -10,6 +10,8 @@ const initialTodos = [
   "🥥 Coconut",
 ];
 
+let counter = 0;
+
 export default function Todos() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState(initialTodos);
@@ -24,18 +26,16 @@ export default function Todos() {
     <>
       <form onSubmit={handleSubmit} className="flex flex-col">
         <input
-          type="text"
-          id="todo"
-          name="todo"
           className="h-20 rounded-3xl py-3.5 px-8 text-2xl shadow dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none"
           value={todo}
           onChange={(event) => setTodo(event.target.value)}
           placeholder="Add todo..."
         />
       </form>
-      {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} />
-      ))}
+      {todos.map((todo, index) => {
+        counter++;
+        return <Todo key={counter} initialTodo={todo} />;
+      })}
     </>
   );
 }
