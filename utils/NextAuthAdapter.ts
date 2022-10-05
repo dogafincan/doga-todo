e36bc80461aa8bb2e53666ad270b9fdc45dea07d
Doga Fincan <doga@icloud.com>
2022-10-05 09:15:58 +0200

    Refactor NextAuthAdapter.ts

--- utils/NextAuthAdapter.ts
+++ utils/NextAuthAdapter.ts
@@ -6,20 +6,20 @@ export function XataAdapter(client: XataClient): Adapter {
   return {
     async createUser(user) {
       const newUser = await client.db.nextauth_users.create(user);
-      return { emailVerified: newUser.emailVerified ?? null, id: newUser.id };
+      return { ...newUser, emailVerified: newUser.emailVerified ?? null };
     },
     async getUser(id) {
       const user = await client.db.nextauth_users.filter({ id }).getFirst();
 
       return user
-        ? { ...user, emailVerified: user.emailVerified ?? null, id: user.id }
+        ? { ...user, emailVerified: user.emailVerified ?? null }
         : null;
     },
     async getUserByEmail(email) {
       const user = await client.db.nextauth_users.filter({ email }).getFirst();
 
       return user
-        ? { ...user, emailVerified: user.emailVerified ?? null, id: user.id }
+        ? { ...user, emailVerified: user.emailVerified ?? null }
         : null;
     },
     async getUserByAccount({ providerAccountId, provider }) {
@@ -33,7 +33,7 @@ export function XataAdapter(client: XataClient): Adapter {
 
       const user = result?.user;
       return user
-        ? { ...user, emailVerified: user.emailVerified ?? null, id: user.id }
+        ? { ...user, emailVerified: user.emailVerified ?? null }
         : null;
     },
     async updateUser(user) {
@@ -60,14 +60,17 @@ export function XataAdapter(client: XataClient): Adapter {
     },
     async createSession(initialSession) {
       const { userId, ...session } = initialSession;
+
       const newXataSession = await client.db.nextauth_sessions.create({
         ...session,
         user: { id: userId },
       });
+
       await client.db.nextauth_users_sessions.create({
         user: { id: userId },
         session: { id: newXataSession.id },
       });
+
       return { ...newXataSession, ...session, userId };
     },
     async getSessionAndUser(sessionToken) {
@@ -75,6 +78,7 @@ export function XataAdapter(client: XataClient): Adapter {
         .select(["user.*", "session.*"])
         .filter({ "session.sessionToken": sessionToken })
         .getFirst();
+
       if (!result?.session || !result?.user) {
         return null;
       }
@@ -96,11 +100,13 @@ export function XataAdapter(client: XataClient): Adapter {
       const session = await client.db.nextauth_sessions
         .filter({ sessionToken })
         .getFirst();
+
       if (!session) {
         return null;
       }
 
       await client.db.nextauth_sessions.update({ ...session, ...data });
+
       return {
         ...session,
         sessionToken,
@@ -116,15 +122,19 @@ export function XataAdapter(client: XataClient): Adapter {
       const session = await client.db.nextauth_sessions
         .filter({ sessionToken })
         .getFirst();
+
       if (!session) {
         return;
       }
+
       const connectedSession = await client.db.nextauth_users_sessions
         .filter({ "session.sessionToken": sessionToken })
         .getFirst();
+
       if (!connectedSession) {
         return;
       }
+
       await client.db.nextauth_sessions.delete(session.id);
       await client.db.nextauth_users_sessions.delete(connectedSession.id);
     },
