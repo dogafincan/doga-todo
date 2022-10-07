a00e8189568d9a320139f326c27319da42b0772f
Doga Fincan <doga@icloud.com>
2022-10-07 20:09:11 +0200

    move div element inside LoginButton component

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -13,20 +13,22 @@ const LoginButton = () => {
   };
 
   return (
-    <button
-      disabled={status === "loading"}
-      className={`mt-2 rounded-md bg-neutral-800 px-4 py-2 text-white shadow duration-200 ease-linear hover:bg-neutral-700 focus:outline-0 focus:ring-4 focus:ring-blue-400 motion-reduce:transition-colors sm:px-10 ${
-        status === "loading" && "animate-pulse"
-      }`}
-      onClick={status === "authenticated" ? handleSignOut : handleSignIn}
-    >
-      <p className="flex items-center gap-x-2">
-        <FaGithub />
-        {status === "authenticated" && "Sign out"}
-        {status === "loading" && "Loading..."}
-        {status === "unauthenticated" && "Continue with GitHub"}
-      </p>
-    </button>
+    <div>
+      <button
+        disabled={status === "loading"}
+        className={`mt-2 rounded-md bg-neutral-800 px-4 py-2 text-white shadow duration-200 ease-linear hover:bg-neutral-700 focus:outline-0 focus:ring-4 focus:ring-blue-400 motion-reduce:transition-colors sm:px-10 ${
+          status === "loading" && "animate-pulse"
+        }`}
+        onClick={status === "authenticated" ? handleSignOut : handleSignIn}
+      >
+        <p className="flex items-center gap-x-2">
+          <FaGithub />
+          {status === "authenticated" && "Sign out"}
+          {status === "loading" && "Loading..."}
+          {status === "unauthenticated" && "Continue with GitHub"}
+        </p>
+      </button>
+    </div>
   );
 };
 
