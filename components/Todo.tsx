d05712d7daff1185ec36d098e7e1b329bf77f80f
Doga Fincan <doga@icloud.com>
2022-10-09 21:01:11 +0200

    Remove unused AnimatePresence

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { Todos } from "@utils/xata";
 import useEditTodo from "@utils/useEditTodo";
 import useDeleteTodo from "@utils/useDeleteTodo";
@@ -90,58 +90,56 @@ const Todo = ({
   };
 
   return (
-    <AnimatePresence>
-      <motion.li
-        layout
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.4 }}
-        exit={{ opacity: 0 }}
-        onClick={handleTitleClick}
+    <motion.li
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+      exit={{ opacity: 0 }}
+      onClick={handleTitleClick}
+    >
+      <div
+        className={`flex h-20 cursor-text items-center rounded-3xl bg-white pl-5 shadow duration-200 ease-linear motion-reduce:transition-colors dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none sm:py-3.5 sm:pl-8 ${
+          (active || edit) && "ring-4 ring-blue-400 dark:ring-blue-500"
+        }`}
       >
-        <div
-          className={`flex h-20 cursor-text items-center rounded-3xl bg-white pl-5 shadow duration-200 ease-linear motion-reduce:transition-colors dark:border dark:border-slate-50/10 dark:bg-neutral-700/40 dark:shadow-none sm:py-3.5 sm:pl-8 ${
-            (active || edit) && "ring-4 ring-blue-400 dark:ring-blue-500"
-          }`}
-        >
-          <input
-            type="checkbox"
-            name="complete-todo"
-            role="button"
-            aria-pressed="false"
-            aria-label="Mark completed"
-            className="mr-4 h-7 w-7 appearance-none rounded-full border-2 border-slate-400/50 bg-slate-200/25 duration-200 ease-linear focus:border-spacing-0 focus:border-0 focus:outline-0 focus:ring-2 focus:ring-blue-400 focus:ring-offset-0 motion-reduce:transition-colors dark:border-slate-50/10 dark:bg-neutral-700/40 focus:dark:ring-blue-500 sm:mr-6"
-            onChange={handleCheckboxChange}
-            onFocus={handleCheckboxFocus}
-            onBlur={handleCheckboxBlur}
-          />
-          {edit ? (
-            <form onSubmit={handleTitleSubmit} className="flex flex-1">
-              <input
-                type="text"
-                name="edit-todo"
-                autoComplete="off"
-                aria-label="Todo title"
-                ref={inputRef}
-                className="flex flex-1 appearance-none rounded-r-3xl border-0 bg-transparent p-0 py-3.5 pr-4 text-base focus:border-0 focus:ring-0 sm:text-xl"
-                value={title}
-                onChange={(event) => setTitle(event.target.value)}
-                placeholder="Add title"
-                onBlur={handleTitleBlur}
-              />
-            </form>
-          ) : (
-            <div
-              className="pr-5 sm:pr-10"
-              onFocus={handleTitleFocus}
-              tabIndex={0}
-            >
-              <p className="line-clamp-2">{title}</p>
-            </div>
-          )}
-        </div>
-      </motion.li>
-    </AnimatePresence>
+        <input
+          type="checkbox"
+          name="complete-todo"
+          role="button"
+          aria-pressed="false"
+          aria-label="Mark completed"
+          className="mr-4 h-7 w-7 appearance-none rounded-full border-2 border-slate-400/50 bg-slate-200/25 duration-200 ease-linear focus:border-spacing-0 focus:border-0 focus:outline-0 focus:ring-2 focus:ring-blue-400 focus:ring-offset-0 motion-reduce:transition-colors dark:border-slate-50/10 dark:bg-neutral-700/40 focus:dark:ring-blue-500 sm:mr-6"
+          onChange={handleCheckboxChange}
+          onFocus={handleCheckboxFocus}
+          onBlur={handleCheckboxBlur}
+        />
+        {edit ? (
+          <form onSubmit={handleTitleSubmit} className="flex flex-1">
+            <input
+              type="text"
+              name="edit-todo"
+              autoComplete="off"
+              aria-label="Todo title"
+              ref={inputRef}
+              className="flex flex-1 appearance-none rounded-r-3xl border-0 bg-transparent p-0 py-3.5 pr-4 text-base focus:border-0 focus:ring-0 sm:text-xl"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+              placeholder="Add title"
+              onBlur={handleTitleBlur}
+            />
+          </form>
+        ) : (
+          <div
+            className="pr-5 sm:pr-10"
+            onFocus={handleTitleFocus}
+            tabIndex={0}
+          >
+            <p className="line-clamp-2">{title}</p>
+          </div>
+        )}
+      </div>
+    </motion.li>
   );
 };
 
