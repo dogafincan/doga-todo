06899451e244c3c2dcb11f7fc521a1e4fa3babf9
Doga Fincan <doga@icloud.com>
2022-10-14 14:28:14 +0200

    Add react-query-devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { LazyMotion, domMax, MotionConfig } from "framer-motion";
 import createIDBPersister from "@utils/createIDBPersister";
 import "@styles/globals.css";
@@ -22,6 +23,7 @@ if (typeof window !== "undefined") {
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <SessionProvider>
         <MotionConfig reducedMotion="user" transition={{ duration: 0.2 }}>
           <LazyMotion features={domMax} strict>
