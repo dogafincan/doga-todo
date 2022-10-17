d4d7a1b4d34316353fc47d2aa13054eb6e64a0b4
Doga Fincan <doga@icloud.com>
2022-10-18 01:31:44 +0200

    Add scale animation

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -104,9 +104,9 @@ const Todo = ({
     <m.li
       layout
       key={todo.id}
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 100 }}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 0.2 }}
       onClick={handleTitleClick}
     >
