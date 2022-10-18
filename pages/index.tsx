a0dce56500d4ce21f0b38b2300f14602bf3a79e1
Doga Fincan <doga@icloud.com>
2022-10-18 14:15:37 +0200

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import cookie from "cookie";
