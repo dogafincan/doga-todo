b477df0f8604aaa12b5f60ec6cb81a24d0f0e052
Doga Fincan <doga@icloud.com>
2022-10-23 01:03:55 +0200

    Test against mobile viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,18 +73,18 @@ const config: PlaywrightTestConfig = {
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: {
+        ...devices["Pixel 5"],
+      },
+    },
+    {
+      name: "Mobile Safari",
+      use: {
+        ...devices["iPhone 12"],
+      },
+    },
 
     /* Test against branded browsers. */
     // {
@@ -107,7 +107,6 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    // url: baseURL,
     port: 3000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
